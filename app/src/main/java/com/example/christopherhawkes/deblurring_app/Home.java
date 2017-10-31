713b919a04eaf59187c2751d2e69ac50cc39271f
Chris Hawkes <chawkesmexico@gmail.com>
2017-10-31 14:38:08 +0000

    [CH] Camera simulator.

--- app/src/main/java/com/example/christopherhawkes/deblurring_app/Home.java
+++ app/src/main/java/com/example/christopherhawkes/deblurring_app/Home.java
@@ -1,10 +1,21 @@
 package com.example.christopherhawkes.deblurring_app;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class Home extends AppCompatActivity {
+    private int TAKE_PICTURE_REQUEST_CODE = 0;
+    VideoView imageView;
+    Button takePic;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -16,9 +27,30 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        // Example of a call to a native method
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
+
+        takePic = (Button) findViewById(R.id.button3);
+        imageView = (VideoView) findViewById(R.id.videoView);
+
+        takePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePicIntent = new Intent();
+                takePicIntent.setAction(MediaStore.ACTION_VIDEO_CAPTURE);
+
+                startActivityForResult(takePicIntent, TAKE_PICTURE_REQUEST_CODE);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == TAKE_PICTURE_REQUEST_CODE && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            imageView.setVideoURI(imageUri);
+            imageView.start();
+        }
     }
 
     /**
