05e6435da94055dd9cf11624641950ba71251079
Chris Hawkes <chawkesmexico@gmail.com>
2017-11-02 09:39:42 +0000

    [CH] Upload stored images with error messages.

--- app/src/main/java/com/example/christopherhawkes/deblurring_app/Home.java
+++ app/src/main/java/com/example/christopherhawkes/deblurring_app/Home.java
@@ -2,6 +2,8 @@ package com.example.christopherhawkes.deblurring_app;
 
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -11,14 +13,18 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import org.tensorflow.TensorFlow;
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class Home extends AppCompatActivity {
     private int TAKE_PICTURE_REQUEST_CODE = 0;
-    VideoView imageView;
+    ImageView imageView;
     Button takePic;
 
     // Used to load the 'native-lib' library on application startup.
@@ -36,19 +42,21 @@ public class Home extends AppCompatActivity {
 //        tv.setText(stringFromJNI());
 
         takePic = (Button) findViewById(R.id.button3);
-        imageView = (VideoView) findViewById(R.id.videoView);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         takePic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent takePicIntent = new Intent();
-                takePicIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+//                Intent takePicIntent = new Intent();
+//                takePicIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+//
+//                startActivityForResult(takePicIntent, TAKE_PICTURE_REQUEST_CODE);
 
-                startActivityForResult(takePicIntent, TAKE_PICTURE_REQUEST_CODE);
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, TAKE_PICTURE_REQUEST_CODE);
             }
         });
-
-
 
         /** One time initialization: */
         AssetManager assetManager = getAssets();
@@ -70,11 +78,23 @@ public class Home extends AppCompatActivity {
         System.out.println("fetched the output " + output[0]);
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == TAKE_PICTURE_REQUEST_CODE && resultCode == RESULT_OK) {
-            Uri imageUri = data.getData();
-            imageView.setVideoURI(imageUri);
-            imageView.start();
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+
+        if (reqCode == TAKE_PICTURE_REQUEST_CODE && resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                imageView.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(Home.this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(Home.this, "You haven't picked Image",Toast.LENGTH_LONG).show();
         }
     }
 
