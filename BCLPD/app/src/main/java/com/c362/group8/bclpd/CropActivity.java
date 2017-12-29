88564eea9b240dfca93aa070fc7579d69f14ce92
ramonfmir <rfernandezmir@gmail.com>
2017-12-29 12:53:55 +0100

    [RF] Started a clean project.

---
+++ BCLPD/app/src/main/java/com/c362/group8/bclpd/CropActivity.java
@@ -0,0 +1,13 @@
+package com.c362.group8.bclpd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CropActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop);
+    }
+}
